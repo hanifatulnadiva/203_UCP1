85faf649920a3a7cdb30ed7474adb9de310e198f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-04 09:32:50 +0700

    jika di running akan menampilkan informasi di terminal bahwa "server is running on 3000" sesuai dengan port yang sudah ditentukan lalu akan sinkronisasi database jika database nya sinkron maka akan muncul pesan "server started" jika tidak berhasil terhung maka akan menampilkan pesan err

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended:true
 }));
+
+app.listen(PORT,()=>{
+    console.log (`server is running on ${PORT}`)
+})
+
+db.sequelize.sync()
+.then ((result)=>{
+    app.listen(3000,()=>{
+        console.log('server started');
+    })
+})
+
+.catch((err)=>{
+    console.log(err);
+})
