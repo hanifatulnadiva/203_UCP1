64741f52e9b6701b64b2991f4a651013981134c6
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-04 09:51:57 +0700

    fungsi untuk delete untuk menghapus data film

--- index.js
+++ index.js
@@ -36,9 +36,24 @@ app.post("/film", async (req,res)=>{
     const data= req.body;
     try{
         const film = await db.Film.create(data);
-        res.send({message:"data film berhasil dibuat", film})
+        res.send({message:"data film berhasil dibuat",film})
     }catch(err){
         res.send(err);
     }
 });
 
+
+app.delete("/film/:id", async(req,res)=>{
+    const id=req.params.id;
+    try{
+        const film=await db.Film.findByPk(id);
+        if(!film){
+            return res.status(404).send({Message: "Data film tidak di temukan"});
+        }
+        await film.destroy();
+        res.send({Message:"Data berhasil di hapus"})
+
+    }catch(err){
+        res.status(500).send({message:"terjadi kesalahan saat delete data film", Error:err.message})
+    }
+});
